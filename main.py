6575fca429c1179d97f292e6934355d905df6652
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-03-16 23:28:09 +0300

    initial commit;

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+current_year = datetime.now().year
+established_year = datetime(year=1920, month=1, day=1).year
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+def get_correct_year_ending(year):
+    pass
+
+
+rendered_page = template.render(
+    winery_age=current_year - established_year
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
 server.serve_forever()
